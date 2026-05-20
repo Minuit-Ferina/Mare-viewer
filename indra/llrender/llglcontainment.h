9b9096b6bfb280ceaf5083383b4157ad2a41f6ee
Minuit-Ferina <minuit.ferina@gmail.com>
2026-05-20 23:14:24 +0200

    llrender: add phase 1 GL containment module

---
+++ indra/llrender/llglcontainment.h
@@ -0,0 +1,31 @@
+/**
+ * @file llglcontainment.h
+ * @brief Phase 1 OpenGL containment markers.
+ *
+ * $LicenseInfo:firstyear=2026&license=viewerlgpl$
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * $/LicenseInfo$
+ */
+
+#ifndef LL_LLGLCONTAINMENT_H
+#define LL_LLGLCONTAINMENT_H
+
+namespace LLGLContainment
+{
+// Phase 1 marker only. Add behavior here only through small reviewed tasks.
+const char* getPhaseOneScope();
+}
+
+#endif // LL_LLGLCONTAINMENT_H
